69f2429cebed6ab585487a95dfc7de5acc8d3a74
psb6396 <psb6396@naver.com>
2024-11-18 18:17:04 +0900

    kj

--- src/components/searchResults.jsx
+++ src/components/searchResults.jsx
@@ -5,7 +5,7 @@ import { fetchSearchResults } from '../features/weather/weatherSlice'
 function SearchResults() {
    const [searchParams] = useSearchParams() //쿼리 파라미터 가져오기
    const query = searchParams.get('query') // 쿼리 파라미터 값 추출
-
+   console.log(query)
    const dispatch = useDispatch()
    dispatch(fetchSearchResults({}))
    return
