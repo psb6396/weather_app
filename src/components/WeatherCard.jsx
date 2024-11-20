f04d4c8ced2fd002b7fbdb46ddafea74d4303ffa
psb6396 <psb6396@naver.com>
2024-11-20 18:30:23 +0900

    as

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -13,11 +13,16 @@ export default function WeatherCard({ forecasts_list }) {
          {forecasts_list.map((forecast) => (
             <Card sx={{ maxWidth: 345 }}>
                {forecast.weather.map((element) => (
-                  <p>{element.description}</p>
+                  <p>
+                     {/* {element.description} */}
+                     {/* <CardMedia sx={{ height: 140 }} image={element.icon ? `https://image.tmdb.org/t/p/w400${movie.poster_path}` : '/images/poster.png'} title="....." /> */}
+                     <CardMedia sx={{ height: 140 }} image={element.icon ? `https://openweathermap.org/img/wn/${element.icon}@2x.png` : <p>이미지가 없음</p>} title="....." />
+                  </p>
                ))}
-               <CardMedia sx={{ height: 140 }} image={movie.poster_path ? `https://image.tmdb.org/t/p/w400${movie.poster_path}` : '/images/poster.png'} title="....." />
                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div"></Typography>
+                  <Typography gutterBottom variant="h5" component="div">
+                     {forecast.dt_txt}
+                  </Typography>
                   <Typography gutterBottom variant="body2" component="div">
                      {forecast.weather.map((element) => (
                         <p>{element.description}</p>
