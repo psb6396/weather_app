cea4c7053de324da09236e2bf5040dc8ba8c6c95
psb6396 <psb6396@naver.com>
2024-11-21 15:40:03 +0900

    ss

---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from 'react-router-dom'
+
+function Menu() {
+   return (
+      <header>
+         <nav>
+            <ul></ul>
+         </nav>
+      </header>
+   )
+}
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,18 +1,21 @@
 import * as React from 'react'
 import Card from '@mui/material/Card'
-// import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
-// import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid2'
 
 export default function WeatherCard({ forecasts_list, name }) {
-   return (<>
-         <h1>{name}의 5일치 날씨</h1>
-      <Grid container spacing={2.5}>
-         {forecasts_list.map((forecast) =>{ 
-            if(forecast !== null)(
+   var hour = null
+   return (
+      <>
+         <h1>{name}의 5일치 날씨(12시기준)</h1>
+         <Grid container spacing={2.5}>
+            {/* {forecasts_list
+               .forEach((forecast) => {
+                  forecast.weather.filter((element) => element.description === '맑음')
+               })
+               .map((forecast) => (
                   <Card sx={{ maxWidth: 345 }}>
                      {forecast.weather.map((element) => (
                         <p>
@@ -28,38 +31,38 @@ export default function WeatherCard({ forecasts_list, name }) {
                         <Typography gutterBottom variant="h5" component="div">
                            {forecast.dt_txt}
                         </Typography>
-                        
-                        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                           
-                        </Typography>
+
+                        <Typography variant="body2" sx={{ color: 'text.secondary' }}></Typography>
                      </CardContent>
-                  </Card>)}
-               
-         )  }
-         {/* {forecasts_list.forEach((forecast)=>(
-            <Card sx={{ maxWidth: 345 }}>
-               {forecast.weather.map((element) => (
-                  <p>
-                     <CardMedia sx={{ height: 140 }} image={element.icon ? `https://openweathermap.org/img/wn/${element.icon}@2x.png` : <p>이미지가 없음</p>} title="....." />
-                  </p>
-               ))}
-               <CardContent>
-                  <Typography gutterBottom variant="body2" component="div">
-                     {forecast.weather.map((element) => (
-                        <p>{element.description}</p>
-                     ))}
-                  </Typography>
-                  <Typography gutterBottom variant="h5" component="div">
-                     {forecast.dt_txt}
-                  </Typography>
-                  
-                  <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                     
-                  </Typography>
-               </CardContent>
-            </Card>
-         ))} */}
-      </Grid>
-   </>
+                  </Card>
+               ))} */}
+            {forecasts_list.map((forecast) => {
+               hour = parseInt(forecast.dt_txt.split(' ')[1].split(':')[0], 10)
+               if (hour === 12) {
+                  return (
+                     <Card sx={{ maxWidth: 345 }}>
+                        {forecast.weather.map((element) => (
+                           <p>
+                              <CardMedia sx={{ height: 140 }} image={element.icon ? `https://openweathermap.org/img/wn/${element.icon}@2x.png` : <p>이미지가 없음</p>} title="....." />
+                           </p>
+                        ))}
+                        <CardContent>
+                           <Typography gutterBottom variant="body2" component="div">
+                              {forecast.weather.map((element) => (
+                                 <p>{element.description}</p>
+                              ))}
+                           </Typography>
+                           <Typography gutterBottom variant="h5" component="div">
+                              {forecast.dt_txt}
+                           </Typography>
+
+                           <Typography variant="body2" sx={{ color: 'text.secondary' }}></Typography>
+                        </CardContent>
+                     </Card>
+                  )
+               }
+            })}
+         </Grid>
+      </>
    )
 }
